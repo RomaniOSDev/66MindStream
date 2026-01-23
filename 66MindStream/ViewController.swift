e037c0a91ecbdba6d2f457bd38079e941e34b06b
Roman <romazan.26@gmail.com>
2026-01-23 10:22:49 +0300

    Initial Commit

---
+++ 66MindStream/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  66MindStream
+//
+//  Created by Роман Главацкий on 23.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
